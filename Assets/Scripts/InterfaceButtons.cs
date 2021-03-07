cef78e41a97a7b973b1bd7d2df29d6ebe025167d
Laura <lchin039@uottawa.ca>
2021-03-06 19:29:30 -0500

    Added Button to go to search screen

---
+++ Assets/Scripts/InterfaceButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterfaceButtons : MonoBehaviour
+{
+    public void toSorting()
+    {
+        SceneManager.LoadScene("Manual Search Screen");
+    }
+
+    public void toMenu()
+    {
+        SceneManager.LoadScene("Unity Project Save 2");
+    }
+}
