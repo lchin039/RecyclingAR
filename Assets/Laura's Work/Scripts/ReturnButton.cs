7b3a5aec7a5d27bf50674d9449e0810ead376d7e
Laura <lchin039@uottawa.ca>
2021-03-09 11:25:17 -0500

    Implemented a search bar (sort of)
    has a textbox that outputs the typed items
    Some small updates where I moved all my scenes and scripts into a folder

---
+++ Assets/Laura's Work/Scripts/ReturnButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnButton : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        string place = "Manual Search Screen";
+        SceneManager.LoadScene(place);
+    }
+    
+}
