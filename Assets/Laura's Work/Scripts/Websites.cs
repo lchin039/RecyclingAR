8b39817c01791a6a7f78f6fd7e7fc83c97271e97
Laura <lchin039@uottawa.ca>
2021-03-22 16:30:56 -0400

    Working on Website Page

---
+++ Assets/Laura's Work/Scripts/Websites.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Websites : MonoBehaviour
+{
+    public static void OurSite()
+    {
+        Application.OpenURL("https://shair086.wixsite.com/areyourecycling");
+    }
+
+
+}
