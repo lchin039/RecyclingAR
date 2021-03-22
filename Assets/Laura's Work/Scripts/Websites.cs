ae9c7b8de09de1bb77820363000bd650ca4bbb4e
Laura <lchin039@uottawa.ca>
2021-03-22 17:24:09 -0400

    Added Websites to Education

--- Assets/Laura's Work/Scripts/Websites.cs
+++ Assets/Laura's Work/Scripts/Websites.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 
 public class Websites : MonoBehaviour
 {
+
     public static void OurSite()
     {
         Application.OpenURL("https://shair086.wixsite.com/areyourecycling");
+    }
+
+    public void uOttawa()
+    {
+        Application.OpenURL("https://www.uottawa.ca/en");
+    }
+
+    public void Ottawa()
+    {
+        Application.OpenURL("https://ottawa.ca/en/garbage-and-recycling/recycling");
+    }
+
+    public void TerraCycle()
+    {
+        Application.OpenURL("https://www.terracycle.com/en-CA/");
     }
 
 
